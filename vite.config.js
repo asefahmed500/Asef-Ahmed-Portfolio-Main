6647b2bf389c2824316cb344d05f42f823de98ee
Asef Ahmed <asefahmed500@gmail.com>
2024-12-11 22:38:04 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensures the server binds to all network interfaces
+    port: process.env.PORT || 5173 // Use the PORT environment variable if available, fallback to 5173
+  }
 })
